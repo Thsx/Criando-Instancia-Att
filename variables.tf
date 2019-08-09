7c899be3937c710d370d6c24d50bce59d9bd38ce
Thsx <not_max2@hotmail.com>
2019-08-09 10:45:49 -0300

    adicionando variables com atualizações

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "region" {
+    default = "us-west-1"
+    description = "Main region"
+}
+
+variable  "ami"{
+    default = "ami-056ee704806822732"
+}
+
+variable "type"{
+    default ="t2.micro" 
+}
+
+variable "cidr_block_vpc"{
+    default = "192.168.0.0/16"
+}
+
+variable "cidr_block_public" {
+  default = "192.168.1.0/24"
+}
+variable "cidr_block_private" {
+  default = "192.168.2.0/24"
+}
+variable "port" {
+    default = "80"
+}
+
+variable "i_protocol"{
+    default = "tcp"
+}
