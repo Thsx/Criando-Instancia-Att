480d67c18af885980f7e8a62338a04b6dbd707c2
Thsx <50934142+Thsx@users.noreply.github.com>
2019-08-12 10:32:07 -0300

    Mudanças main

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-bucket-thiagod"
-    key    = "terraform.tfstate"
-    region = "us-west-1"
-  }
-}
 provider "aws" {
 region      = "${var.region}"
 }
